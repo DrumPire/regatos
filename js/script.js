0376aa096efb0bb1a2e3491cf68a42c4b2f5e96b
Alex <expresproject@gmail.com>
2020-10-09 20:24:56 +0300

    fixed responsive 1024)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const burgerButton = document.querySelector('.burger');
+
+
+burgerButton.addEventListener('click', () => {
+  console.log('click');
+});
+
+
 var mySwiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.slider-next',
