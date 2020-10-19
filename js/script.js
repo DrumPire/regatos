889757d512f4fe1e025307a2d057019931fbdbb8
Alex <expresproject@gmail.com>
2020-10-19 18:30:50 +0300

    fixed responsive, add overlay

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const burgerButton = document.querySelector('.burger');
 const mobile = document.querySelector('.mobile');
 const mobileClose = document.querySelector('.mobile-close');
+const overlay = document.querySelector('.overlay');
 
 
 burgerButton.addEventListener('click', () => {
   mobile.classList.add('open');
+  overlay.classList.add('active');
 });
 
 mobileClose.addEventListener('click', () => {
   mobile.classList.remove('open');
+  overlay.classList.remove('active');
 });
+
+overlay.addEventListener('click', () => {
+  mobile.classList.remove('open');
+  overlay.classList.remove('active');
+})
 
 
 var mySwiper = new Swiper('.swiper-container', {
